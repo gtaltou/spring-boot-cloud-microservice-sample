f0d5b1d4a5e8718eb925f5c8d770603df8516dbd
Guy TALTOU <guy.taltou@gmail.com>
2021-04-10 22:51:39 +0200

    first config

---
+++ config-service/src/test/java/com/traltou/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traltou.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
