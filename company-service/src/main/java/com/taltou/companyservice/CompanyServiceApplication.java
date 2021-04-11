62de22ae68a375da56fa0ec1d2409aba98ce166d
Guy TALTOU <guy.taltou@gmail.com>
2021-04-11 15:57:44 +0200

    Update of company-service

--- company-service/src/main/java/com/taltou/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/com/taltou/companyservice/CompanyServiceApplication.java
@@ -5,10 +5,12 @@ import com.taltou.companyservice.entities.Company;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.stream.Stream;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CompanyServiceApplication {
 
