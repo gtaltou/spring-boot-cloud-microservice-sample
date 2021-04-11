322100b96d5a21d383a012c728cc1af012b9a242
Guy TALTOU <guy.taltou@gmail.com>
2021-04-11 16:00:59 +0200

    Adding proxy-service

---
+++ proxy-service/src/test/java/com/taltou/proxyservice/ProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taltou.proxyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
